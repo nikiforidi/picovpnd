d05976e3f9476780a6639400b92a48d63e26d888
anatolio-deb <nikiforova693@gmail.com>
2025-05-15 07:04:21 +0000

    debug

--- daemon/main.go
+++ daemon/main.go
@@ -22,18 +22,21 @@ const SALT_SIZE = 16
 var HOSTNAME string
 
 func main() {
-	server, err := net.Listen("tcp", "picovpn.ru:5000")
-	if err != nil {
+	if err := userAdd("test", "12345678"); err != nil {
 		panic(err)
 	}
-	defer server.Close()
-	for {
-		connection, err := server.Accept()
-		if err != nil {
-			panic(err)
-		}
-		go handler(connection)
-	}
+	// server, err := net.Listen("tcp", "picovpn.ru:5000")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer server.Close()
+	// for {
+	// 	connection, err := server.Accept()
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	go handler(connection)
+	// }
 }
 
 func handler(connection net.Conn) {
