8f2dbc11cb99c1ddbc74107b83da0e28b8dc3cc5
anatolio-deb <nikiforova693@gmail.com>
2025-05-17 17:42:31 +0000

    refactor

--- common/api.go
+++ common/api.go
@@ -54,10 +54,19 @@ func PayloadDispatcher(req Request) error {
 	logrus.Infof("Request payload: %v", req.Payload)
 	switch req.Method {
 	case UserAdd:
-		p, ok := req.Payload.(UserAddPayload)
+		payload, ok := req.Payload.(map[string]string)
 		if ok {
-			logrus.Infof("Request create user %s", p.Username)
-			return ocserv.UserAdd(p.Username, p.Password)
+			username, ok := payload["username"]
+			if !ok {
+				return fmt.Errorf("bad request: %s", req.Method)
+			}
+			logrus.Infof("Request create user %s", username)
+			password, ok := payload["password"]
+			if !ok {
+				return fmt.Errorf("bad request: %s", req.Method)
+			}
+
+			return ocserv.UserAdd(username, password)
 		} else {
 			return fmt.Errorf("bad request: %s", req.Method)
 		}
