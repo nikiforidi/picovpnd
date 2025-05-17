36efce01c23c168f3ee4b83e8f5f2751e0c5997f
anatolio-deb <nikiforova693@gmail.com>
2025-05-17 07:26:36 +0000

    roll back pssw algo

--- ocserv/main.go
+++ ocserv/main.go
@@ -1,44 +1,40 @@
 package ocserv
 
 import (
-	"fmt"
-	"os"
 	"os/exec"
-	"time"
 
-	expect "github.com/Netflix/go-expect"
 	"github.com/sirupsen/logrus"
 )
 
 // ocpasswd -c /etc/ocserv/ocpasswd username
 func UserAdd(username, password string) error {
-	// return cryptInt("/etc/ocserv/ocpasswd", username, "*", password)
-	c, err := expect.NewConsole(expect.WithStdout(os.Stdout))
-	if err != nil {
-		return err
-	}
-	defer c.Close()
+	return cryptInt("/etc/ocserv/passwd", username, "*", password)
+	// c, err := expect.NewConsole(expect.WithStdout(os.Stdout))
+	// if err != nil {
+	// 	return err
+	// }
+	// defer c.Close()
 
-	cmd := exec.Command("ocpasswd", "-c", "/etc/ocserv/ocpasswd", username)
-	cmd.Stdin = c.Tty()
-	cmd.Stdout = c.Tty()
-	cmd.Stderr = c.Tty()
+	// cmd := exec.Command("ocpasswd", "-c", "/etc/ocserv/ocpasswd", username)
+	// cmd.Stdin = c.Tty()
+	// cmd.Stdout = c.Tty()
+	// cmd.Stderr = c.Tty()
 
-	go func() {
-		c.ExpectEOF()
-	}()
+	// go func() {
+	// 	c.ExpectEOF()
+	// }()
 
-	err = cmd.Start()
-	if err != nil {
-		return err
-	}
+	// err = cmd.Start()
+	// if err != nil {
+	// 	return err
+	// }
 
-	time.Sleep(time.Second)
-	c.Send(fmt.Sprintf("%s\x1b", password))
-	time.Sleep(time.Second)
-	c.Send(fmt.Sprintf("%s\x1b", password))
+	// time.Sleep(time.Second)
+	// c.Send(fmt.Sprintf("%s\x1b", password))
+	// time.Sleep(time.Second)
+	// c.Send(fmt.Sprintf("%s\x1b", password))
 
-	return cmd.Wait()
+	// return cmd.Wait()
 }
 
 func UserLock(username string) error {
