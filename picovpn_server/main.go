0216a60fa1e83c6f55f5056ee310c5f5b6d8c67b
anatolio-deb <nikiforova693@gmail.com>
2025-06-11 15:18:47 +0000

    tls

--- picovpn_server/main.go
+++ picovpn_server/main.go
@@ -18,10 +18,6 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
-var (
-	port = flag.Int("port", 50051, "The server port")
-)
-
 // server is used to implement helloworld.GreeterServer.
 type server struct {
 	pb.OpenConnectServiceServer
@@ -92,33 +88,29 @@ func (s *server) UserChangePassword(context.Context, *pb.UserChangePasswordReque
 }
 
 func main() {
-	// flag.Parse()
-	// lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	// if err != nil {
-	// 	log.Fatalf("failed to listen: %v", err)
-	// }
-	// s := grpc.NewServer()
-	// pb.RegisterOpenConnectServiceServer(s, &server{})
-	// log.Printf("server listening at %v", lis.Addr())
-	// if err := s.Serve(lis); err != nil {
-	// 	log.Fatalf("failed to serve: %v", err)
-	// }
-	flag.Parse()
-
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
 	certManager := autocert.Manager{
 		Prompt:     autocert.AcceptTOS,
 		HostPolicy: autocert.HostWhitelist(os.Getenv("AUTOCERT_DOMAIN")), // Use your email or domain here
 		Cache:      autocert.DirCache(os.Getenv("AUTOCERT_DIR")),         // Directory to cache certificates
 	}
-	config := &tls.Config{GetCertificate: certManager.GetCertificate, MinVersion: tls.VersionTLS12}
+
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", ":0") // Listen on a random port
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 
 	// Create tls based credential.
-	creds, err := credentials.NewServerTLSFromCert()
+	cert, err := certManager.GetCertificate(&tls.ClientHelloInfo{
+		ServerName: os.Getenv("AUTOCERT_DOMAIN"),
+	})
+
+	if err != nil {
+		log.Fatalf("failed to get certificate: %v", err)
+	}
+	// Create credentials from the certificate.
+	creds := credentials.NewServerTLSFromCert(cert)
 	if err != nil {
 		log.Fatalf("failed to create credentials: %v", err)
 	}
